6a542d2ada33b7c1403dfe643376c0c861d94d75
Leyder Quintero <lele.quinter@gmail.com>
2022-07-12 19:34:42 -0500

    Agregando comentarios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,12 @@ export class AppComponent {
   newMemberName = "";
   members: string[] = [];
 
+  // Le asignamos el valor del .value de la input a la constante
   onInput(member: string){
     this.newMemberName = member;
   }
 
+  // Agregamos el nuevo nombre al arreglo members
   addMember(){
     this.members.push(this.newMemberName);
     console.log(this.members);
