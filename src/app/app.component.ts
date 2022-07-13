b84f06a4ae1c74fa4cb46f9691c9c333bea3cca7
Leyder Quintero <lele.quinter@gmail.com>
2022-07-12 20:54:40 -0500

    Attribute directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,21 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  newMemberName = "";
+  newMemberName = '';
   members: string[] = [];
+  errorMessage = '';
 
   // Le asignamos el valor del .value de la input a la constante
-  onInput(member: string){
+  onInput(member: string) {
     this.newMemberName = member;
   }
 
   // Agregamos el nuevo nombre al arreglo members
-  addMember(){
+  addMember() {
+    // Verificacion de string vacio, si el string está vacío no añade al members
+    if(!this.newMemberName){
+      this.errorMessage =  "Name can't be empty"
+      return
+    }
+
     this.members.push(this.newMemberName);
-    console.log(this.members);
-    
+    this.newMemberName = '';
+    this.errorMessage = '';
   }
 }
